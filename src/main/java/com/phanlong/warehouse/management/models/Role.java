cc51f6bde77bca461f25e6f6fb61798e6f4438ed
viettrung317 <leviettrung31072001@gmail.com>
2024-11-13 22:39:46 +0700

    first commit

---
+++ src/main/java/com/phanlong/warehouse/management/models/Role.java
@@ -0,0 +1,31 @@
+package com.phanlong.warehouse.management.models;
+
+public class Role {
+	private int roleId;
+	private String role;
+	
+	public Role(int roleId, String role) {
+		super();
+		this.roleId = roleId;
+		this.role = role;
+	}
+
+	public int getRoleId() {
+		return roleId;
+	}
+
+	public void setRoleId(int roleId) {
+		this.roleId = roleId;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
+	
+}
