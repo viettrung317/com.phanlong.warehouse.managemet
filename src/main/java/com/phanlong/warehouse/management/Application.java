9d90631f879b6843caf4346e21df27bdeea7c643
NNTruong <NNTruong@DESKTOP-AIRBOMD>
2024-11-23 18:08:32 +0700

    test 9:33

--- src/main/java/com/phanlong/warehouse/management/Application.java
+++ src/main/java/com/phanlong/warehouse/management/Application.java
@@ -7,5 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		System.out.println("Hello World");
+		System.out.println("Hello World 123");
+		System.out.println("test 01");
 	}
 }
